0751f49e015976e368afe2d223c4b38099cc4bc1
lkk223303 <lkk223303@gmail.com>
2022-07-10 12:02:09 +0800

    first commit

---
+++ cmd/web/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/lkk223303/hello-go/pkg/handlers"
+)
+
+const portNumber = ":8080"
+
+func main() {
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lkk223303/hello-go
+
+go 1.17
